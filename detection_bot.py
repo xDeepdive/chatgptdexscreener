d0d99320e0457b2a68a52f39fd17332a29b35f37
xDeepdive <85856862+xDeepdive@users.noreply.github.com>
2024-12-12 18:37:05 +0530

    Update detection_bot.py

--- detection_bot.py
+++ detection_bot.py
@@ -119,5 +119,5 @@ if __name__ == "__main__":
     from threading import Thread
     # Start token fetching in a separate thread
     Thread(target=start_fetching_tokens).start()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
