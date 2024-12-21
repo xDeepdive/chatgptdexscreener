8206d00f08c939f72dd5c9de39a680a8e800d486
xDeepdive <85856862+xDeepdive@users.noreply.github.com>
2024-12-21 19:17:26 +0530

    Update detection_bot.py

--- detection_bot.py
+++ detection_bot.py
@@ -110,5 +110,5 @@ if __name__ == "__main__":
     detection_thread.daemon = True
     detection_thread.start()
 
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
